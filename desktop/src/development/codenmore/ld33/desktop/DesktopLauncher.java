ad82e6979d90593c2bf6204eec15c18baeef5396
CodeNMore <jackofall55@gmail.com>
2015-08-21 10:21:58 -0400

    Initial commit, project created

---
+++ desktop/src/development/codenmore/ld33/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package development.codenmore.ld33.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import development.codenmore.ld33.Main;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Main(), config);
+	}
+}
---
+++ html/src/development/codenmore/ld33/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package development.codenmore.ld33.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import development.codenmore.ld33.Main;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new Main();
+        }
+}
